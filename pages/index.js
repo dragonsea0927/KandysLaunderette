ce6273b51e4b46926fbb46315f174417c4ddd6c8
dragonsea0927 <dragonsea927@proton.me>
2021-11-26 00:15:13 +0000

    Created a Our location in the home page.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
@@ -235,6 +236,25 @@ export default function Home() {
 							</div>
 						</div>
 					</div>
+				</div>
+			</div>
+
+			{/* <!--===== OUT STORE LOCATION =====--> */}
+			<div className={styles.storeLocation}>
+				<div className="container">
+					<div className={styles.title}>
+						<h2>Our Location</h2>
+					</div>
+				</div>
+				<div className={styles.storeMap}>
+					{/* <iframe
+						src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9907.997021073143!2d-3.9420236!3d51.6232228!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x919fb6f2312ca92b!2sKandys%20Launderette%20%26%20Dry%20Cleaner!5e0!3m2!1sen!2suk!4v1637884674092!5m2!1sen!2suk"
+						width="600"
+						height="450"
+						style="border:0;"
+						allowfullscreen="true"
+						loading="lazy"
+					></iframe> */}
 				</div>
 			</div>
 
