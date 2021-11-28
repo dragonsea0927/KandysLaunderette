da51a164af85c0bdc0363ed21922021d6ce8e3e2
dragonsea0927 <dragonsea927@proton.me>
2021-11-28 20:00:18 +0000

    Remaned folders and re-structured scss styling file location.

---
+++ components/ContactInfo.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "/styles/Home.module.scss";
+
+const ContactInfo = () => {
+	return (
+		<>
+			{/* <!--===== INFORMATION SECTION =====--> */}
+			<div className={styles.backgroundImageLaundryDryCleaning}>
+				<div className="container">
+					<div className={styles.content}>
+						<div className={styles.title}>
+							<h2>Contact our specialist Team to discuss your request.</h2>
+							<Link href="mailto:KandysLaunderette@gmail.com" target="blank">
+								<a>
+									Email: <span>KandysLaunderette@gmail.com</span>
+								</a>
+							</Link>
+							<Link href="tel:01792 461111" target="blank">
+								<a className={styles.telLinks}>
+									Tel: <span>+44 (0) 1792 46 11 11</span> |
+									<span> 07478 212165</span>
+								</a>
+							</Link>
+							<h5>
+								Opening hours: 8.30am–7pm Monday to Saturday (9am-5pm Sunday)
+							</h5>
+						</div>
+						<div className={styles.button}>
+							<newButton>
+								<Link href="#email" target="blank">
+									<a>Terms & Conditions</a>
+								</Link>
+							</newButton>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default ContactInfo;
--- pages/LaundryDryCleaning.js
+++ pages/LaundryDryCleaning.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import Navbar from "/components/Navbar";
+import ContactInfo from "/components/ContactInfo";
 import Footer from "/components/Footer";
 import styles from "/styles/Home.module.scss";
 
@@ -133,36 +134,7 @@ const LaundryDryCleaning = () => {
 			</div>
 
 			{/* <!--===== INFORMATION SECTION =====--> */}
-			<div className={styles.backgroundImageLaundryDryCleaning}>
-				<div className="container">
-					<div className={styles.content}>
-						<div className={styles.title}>
-							<h2>Contact our specialist Team to discuss your request.</h2>
-							<Link href="mailto:KandysLaunderette@gmail.com" target="blank">
-								<a>
-									Email: <span>KandysLaunderette@gmail.com</span>
-								</a>
-							</Link>
-							<Link href="tel:01792 461111" target="blank">
-								<a className={styles.telLinks}>
-									Tel: <span>+44 (0) 1792 46 11 11</span> |
-									<span> 07478 212165</span>
-								</a>
-							</Link>
-							<h5>
-								Opening hours: 8.30am–7pm Monday to Saturday (9am-5pm Sunday)
-							</h5>
-						</div>
-						<div className={styles.button}>
-							<newButton>
-								<Link href="#email" target="blank">
-									<a>Terms & Conditions</a>
-								</Link>
-							</newButton>
-						</div>
-					</div>
-				</div>
-			</div>
+			<ContactInfo />
 
 			{/* <!--===== FOOTER =====--> */}
 			<Footer />
