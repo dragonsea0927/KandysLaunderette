336bcb6a3c41b97b1b81de99912e0048cec58d00
dragonsea0927 <dragonsea927@proton.me>
2021-12-05 19:38:38 +0000

    Created a resuable storeLocation component and imported it into the our story page.

--- pages/ourStory.js
+++ pages/ourStory.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import NavbarTwo from "/components/NavbarTwo";
+import StoreLocation from "/components/storeLocation";
 import Footer from "/components/Footer";
 import styles from "/styles/Home.module.scss";
 
@@ -181,24 +182,7 @@ const ourStory = () => {
 			</div>
 
 			{/* <!--===== OUT STORE LOCATION =====--> */}
-			<div className={styles.storeLocation}>
-				<div className="container">
-					<div className={styles.title}>
-						<h2>Our Location</h2>
-						<h5>We hope to see you soon</h5>
-					</div>
-				</div>
-				<div className={styles.storeMap}>
-					{/* <iframe
-						src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9907.997021073143!2d-3.9420236!3d51.6232228!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x919fb6f2312ca92b!2sKandys%20Launderette%20%26%20Dry%20Cleaner!5e0!3m2!1sen!2suk!4v1637884674092!5m2!1sen!2suk"
-						width="600"
-						height="450"
-						style="border:0;"
-						allowfullscreen="true"
-						loading="lazy"
-					></iframe> */}
-				</div>
-			</div>
+			<StoreLocation />
 
 			{/* <!--===== FOOTER =====--> */}
 			<Footer />
