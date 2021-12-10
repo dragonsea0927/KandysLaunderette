d38bbb664d07823f88ce77213e82cc70c03a8855
dragonsea0927 <dragonsea927@proton.me>
2021-12-10 21:03:53 +0000

    iFrame error issue check.

--- components/storeLocation.js
+++ components/storeLocation.js
@@ -26,14 +26,13 @@ const storeLocation = () => {
 					</motion.div>
 				</div>
 				<div className={styles.storeMap}>
-					{/* <iframe
+					<iframe
 						src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9907.997021073143!2d-3.9420236!3d51.6232228!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x919fb6f2312ca92b!2sKandys%20Launderette%20%26%20Dry%20Cleaner!5e0!3m2!1sen!2suk!4v1637884674092!5m2!1sen!2suk"
 						width="600"
 						height="450"
 						style="border:0;"
-						allowfullscreen="true"
 						loading="lazy"
-					></iframe> */}
+					></iframe>
 				</div>
 			</div>
 		</>
