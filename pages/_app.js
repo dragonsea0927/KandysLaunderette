a3b3b30dce365434e4f83f5143a5dda42b12a8b4
dragonsea0927 <dragonsea927@proton.me>
2021-12-10 12:17:27 +0000

    Installed framer-motion animation package and imported it into the _app.js app rendering page.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.scss";
+import {AnimatePresence} from "framer-motion";
 
 function MyApp({Component, pageProps}) {
-	return <Component {...pageProps} />;
+	return (
+		<AnimatePresence exitBeforeEnter>
+			<Component {...pageProps} />
+		</AnimatePresence>
+	);
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+import {motion} from "framer-motion";
 import Navbar from "/components/Navbar";
 import StoreLocation from "/components/storeLocation";
 import Footer from "/components/Footer";
