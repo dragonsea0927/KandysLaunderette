27ba07c1a25230ef8332c81e1fff67bd87d42423
dragonsea0927 <dragonsea927@proton.me>
2021-12-11 00:17:21 +0000

    Created Custom reusable Animation Properties/variables for the IndividualService page.

--- pages/LaundryAndDryCleaning/IndividualService.js
+++ pages/LaundryAndDryCleaning/IndividualService.js
@@ -6,6 +6,37 @@ import Navbar from "/components/Navbar";
 import Footer from "/components/Footer";
 import styles from "/styles/Home.module.scss";
 
+// Custom reusable Animation Properties/variables
+const fadeInUp = {
+	initial: {
+		y: 60,
+		opacity: 0,
+	},
+	animate: {
+		y: 0,
+		opacity: 1,
+		transition: {duration: 0.5, ease: "easeOut"},
+	},
+};
+
+const fadeIn = {
+	initial: {
+		opacity: 0,
+	},
+	animate: {
+		opacity: 1,
+		transition: {duration: 1, delay: 0.5, ease: "easeOut"},
+	},
+};
+
+const stagger = {
+	animate: {
+		transition: {
+			staggerChildren: 0.1,
+		},
+	},
+};
+
 const IndividualService = () => {
 	return (
 		<motion.div
