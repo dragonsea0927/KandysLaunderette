d0f5c39af4eef84b11098788155b8c5999d5c851
dragonsea0927 <dragonsea927@proton.me>
2023-02-11 10:17:23 +0000

    Fixed the git file uppercase filename issue in vercel production failed to compile issues.

---
+++ pages/ContactUs.js
@@ -0,0 +1,132 @@
+import {gql} from "@apollo/client";
+import {client} from "../lib/apollo";
+import {motion} from "framer-motion";
+import styles from "/styles/Home.module.scss";
+import ContactInfoMap from "../components/ContactInfoMap";
+import {fadeInUp} from "../animations/animations";
+import {getThemesOptionsContent} from "../lib/themesOptions";
+import {
+	getCommercialServicesMenu,
+	getIndividualServicesMenu,
+} from "../lib/MenuLinks";
+
+// Components
+import Navbar from "/components/Navbar";
+import Footer from "/components/Footer";
+import MetaTag from "../components/Meta/MetaTag";
+
+const contactUs = ({
+	seo,
+	pageTitle,
+	contactUsPageContent,
+	themesOptionsContent,
+	IndividualServicesMenuLinks,
+	CommercialServicesMenuLinks,
+}) => {
+	return (
+		<motion.div
+			exit={{
+				opacity: 0,
+			}}
+			initial="initial"
+			animate="animate"
+		>
+			{/* <!--===== HEAD =====--> */}
+			<MetaTag title={pageTitle} seo={seo} />
+
+			{/* <!--===== NAVBAR =====--> */}
+			<Navbar
+				CommercialServicesMenuLinks={
+					CommercialServicesMenuLinks?.CommercialServicesMenuLinks
+				}
+				IndividualServicesMenuLinks={
+					IndividualServicesMenuLinks?.IndividualServicesMenuLinks
+				}
+			/>
+
+			<main>
+				{/* // <========== BACKGROUND IMAGE ==========> */}
+				<div className={styles.backgroundImageContactUs}></div>
+
+				{/* // <========== PAGE TITLE ==========> */}
+				<div className={styles.pageTitle}>
+					<div>
+						<div className={styles.content}>
+							<motion.div variants={fadeInUp} className={styles.title}>
+								<h2>Contact Us</h2>
+								<h5>Lets get In Touch</h5>
+							</motion.div>
+						</div>
+					</div>
+				</div>
+
+				{/* <!--===== CONTACT US BANNER =====--> */}
+				<ContactInfoMap />
+			</main>
+
+			{/* <!--===== FOOTER =====--> */}
+			<Footer
+				email={themesOptionsContent?.themesOptions?.email}
+				phoneNumber={themesOptionsContent?.themesOptions?.phoneNumber}
+				CommercialServicesMenuLinks={
+					CommercialServicesMenuLinks?.CommercialServicesMenuLinks
+				}
+			/>
+		</motion.div>
+	);
+};
+
+export async function getStaticProps() {
+	const getContactUsPageContent = gql`
+		{
+			pageTitle: pages(where: {id: 175, status: PUBLISH}) {
+				edges {
+					node {
+						title
+					}
+				}
+			}
+			mainContent: pages(where: {id: 175, status: PUBLISH}) {
+				edges {
+					node {
+						seo {
+							fullHead
+						}
+						contactUsPage {
+							heroSection {
+								title
+								subtitle
+								backgroundImage {
+									sourceUrl
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	`;
+
+	const response = await client.query({
+		query: getContactUsPageContent,
+	});
+
+	const CommercialServicesMenuLinks = await getCommercialServicesMenu();
+	const themesOptionsContent = await getThemesOptionsContent();
+	const IndividualServicesMenuLinks = await getIndividualServicesMenu();
+
+	return {
+		props: {
+			themesOptionsContent,
+			CommercialServicesMenuLinks,
+			IndividualServicesMenuLinks,
+			pageTitle: response?.data?.pageTitle?.edges[0]?.node?.title,
+			seo: response?.data?.mainContent?.edges[0]?.node?.seo,
+			contactUsPageContent:
+				response?.data?.mainContent?.edges[0]?.node?.contactUsPage,
+		},
+		revalidate: 1,
+	};
+}
+
+export default contactUs;
